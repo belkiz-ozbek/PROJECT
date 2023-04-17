5143e494445f43acb41c65a8e7c6cedd57de3dfe
ErvaYlmz <130571315+ErvaYlmz@users.noreply.github.com>
2023-04-17 23:17:28 +0300

    Update index.js

--- 206_Project/index.js
+++ 206_Project/index.js
@@ -1,14 +1,15 @@
 /////////////////////// ERVA ////////////////////////
 // Csv file içindeki data ları arraylere okuyoruz
-const classroom={room:null,capacity:null,avaliable:0};
-const busy = {instructor:null,day:null,time:null};
-const service ={code:null,day:null,time:null};
-const courses = {code:null,courseName:null,year:null,credit:null,c_e_state:null,d_c_state:null,num_of_Students:null,instructor:null};
+//var classroom={room:null,capacity:null,avaliable:0};
+//var busy = {instructor:null,day:null,time:null};
+// var service ={code:null,day:null,time:null};
+//var courses = {code:null,courseName:null,year:null,credit:null,c_e_state:null,d_c_state:null,num_of_Students:null,instructor:null};
 
 var arrClassroom=[];
 var arrBusy=[];
 var arrService=[];
 var arrCourses=[];
+var i, k, j, a, temp;
 
 const DaysOfWeek = {
   Monday: 1,
@@ -32,18 +33,26 @@ fetch('./classroom.csv')
     let i=0;
     // Loop over the rows and split each one into cells
     rows.forEach(row => {
-
+      let classroom={room:null,capacity:null,avaliable:0};
       const cells = row.split(';');
       classroom.room=cells[0];
       classroom.capacity=cells [1];
-      classroom.avaliable=[];
+      classroom.avaliable=[[]];
       arrClassroom[i]=classroom;
       i++;
-      
-    });
-    console.log(arrClassroom); 
-        //console log olsa da olur olmasa da olur (sanırım)
+    });console.log(arrClassroom);
   });
+   
+  for(i=0; i<arrClassroom.length; i++){                       //Sorulacak!!!!!!!!!!!
+    for(j=0; j<arrClassroom.length-i-1; j++){
+        if(arrClassroom[j].capacity>arrClassroom[j+1].capacity){
+            temp=arrClassroom[j];
+            arrClassroom[j]=arrClassroom[j+1];
+            arrClassroom[j+1]=temp;
+        }
+    }
+}
+
 //
   fetch('./busy.csv')
   .then(response => response.text())
@@ -51,6 +60,7 @@ fetch('./classroom.csv')
     const rows = data.trim().split('\n');
     i=0;
     rows.forEach(row => {
+      let busy = {instructor:null,day:null,time:null};
       const cells = row.split(';');
       busy.instructor=cells[0];
       busy.day=cells[1];
@@ -68,14 +78,13 @@ fetch('./classroom.csv')
     const rows = data.trim().split('\n');
     i=0;
     rows.forEach(row => {
+      let service ={code:null,day:null,time:null};
       const cells = row.split(';');
       service.code=cells[0];
       service.day=cells[1];
       service.time=cells[2];
       arrService[i]=service;
       i++;
-
-
     });
     
     console.log(arrService);
@@ -88,6 +97,7 @@ fetch('./classroom.csv')
     const rows = data.trim().split('\n');
     i=0;
     rows.forEach(row => {
+      let courses = {code:null,courseName:null,year:null,credit:null,c_e_state:null,d_c_state:null,num_of_Students:null,instructor:null};
       const cells = row.split(';');
       courses.code=cells[0];
       courses.courseName=cells[1];
@@ -105,25 +115,33 @@ fetch('./classroom.csv')
   });
 
 
-function service(){
+function services(){
+  
+    for(k=0; k<arrService.length; k++){     //zorunlu ders yerleştirme
+      for(i=0; i<arrCourses.length; i++){
+        if(arrService[k].code==arrCourses[i].code){
+         //var index=(DaysOfWeek.arrService[k].day)*(TimePeriod.arrService[i].time)-1;
+         
+          for(j=0; j<arrClassroom.length; j++){
+            if(arrClassroom[j].capacity>arrCourses[i].capacity){
+              arrClassroom[j].avaliable[DaysOfWeek.arrService[k].day][TimePeriod.arrService[i].time]=arrCourses[i];
+            }
+          }
 
+          for(j=i; j<arrCourses.length; j++){
+            arrCourses[j]=arrCourses[j+1];
+          }
+        }
+      }
+    }
 
-for(i=0;i<arrService.length;i++){
-let index=DaysOfWeek.arrService[i].day*TimePeriod.arrService[i].time;
+    // for(i=0; i<arrCourses.length; i++){
+    //   for(j=0; j<)
+    // }
 
-
-
-
+    return ;
 }
 
-return ;
 
-}
-
-
-
-
-
-
-
-///////////////////////////////////////
+///Sıralama yapılamadı
+///html e atama sorulacak
